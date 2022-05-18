917a5188e60f5927e51789875685a826d8a034e9
sergiorivera50 <sriveralopez50@gmail.com>
2022-05-18 10:41:20 +0100

    Minor index refactor

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import app from './app'
-import { isProd } from './utils/misc.utils'
 
 const port = Number(process.env.PORT || 8000)
 
-const server = app.listen(port, () => {
-  console.log(`\nServer started on port ${port} 🎉`)
-  if (isProd) console.log('Running as production 🚨')
-})
+app.listen(port, () => console.log(`\nServer started on port ${port} 🎉 (as ${process.env.NODE_ENV})`))
