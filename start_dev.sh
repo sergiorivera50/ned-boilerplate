7375507cce4ef6fbae8b23eee6a98882445760ef
sergiorivera50 <sriveralopez50@gmail.com>
2022-05-18 12:36:34 +0100

    Docker-compose support

--- start_dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-args="--rm --volume=$(pwd):/app --publish=8000:8000"
-
-if [[ -f ".env" ]]; then
-  args+=" --env-file=.env"
-fi
-
-docker run $args $(docker build --quiet --target=builder .)
