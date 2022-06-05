1d0d0daee10d914a019f304eb0b96d904988c93f
sergiorivera50 <sriveralopez50@gmail.com>
2022-06-05 20:20:11 +0100

    404

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,10 @@ const app: Application = express()
 
 import middlewares from './middlewares'
 import routes from './routes'
+import ExpressUtils from './utils/express.utils'
 
 app.use(middlewares)
 app.use(routes)
+app.use('*', (req, res) => ExpressUtils.errorResponse(res, 'Not found', 404))
 
 export default app
