01109a8d10adb97f47cccd8776710c7d29aefaf8
sergiorivera50 <sriveralopez50@gmail.com>
2022-06-10 09:22:34 +0100

    Moved api to /api/v1

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import routes from './routes'
 import ExpressUtils from './utils/express.utils'
 
 app.use(middlewares)
-app.use(routes)
+app.use(`${ExpressUtils.API_ROUTE}`, routes)
 app.use('*', (req, res) => ExpressUtils.errorResponse(res, 'Not found', 404))
 
 export default app
